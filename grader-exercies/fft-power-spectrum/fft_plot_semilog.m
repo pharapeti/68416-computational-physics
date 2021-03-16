a56721ec8f088c44d82b1b46bb66f967203f095f
pharapeti <patrice.harapeti@gmail.com>
2021-03-16 11:45:49 +1100

    semilogy

--- grader-exercies/fft-power-spectrum/fft_plot_semilog.m
+++ grader-exercies/fft-power-spectrum/fft_plot_semilog.m
@@ -8,23 +8,18 @@ fs = 1e3; % Sampling frequency in Hertz
 t = 0:(1/fs):1; % Sampling period
 t = t(1:(end-1)); % Time vector
 N = length(t); % Number of samples
-y = sin(2*pi*100*t); % Signal
-
-subplot(1, 2, 1);
-plot(t, y);
-title('Signal');
+f0 = 100.5; % Initial frequency
+y = sin(2*pi*f0*t);% Signal
 
 Y = fft(y); % FFT computation
 Y = fftshift(Y); % unfold
 Y = Y/N;
 
 dt = mean(diff(t)); % sample spacing
-N = length(t);
 df = 1/(N*dt); % frequency spacing
 fi = (0:(N-1)) - floor(N/2); % unfolded index
 f = df*fi; % frequency vector
 
-subplot(1, 2, 2);
-plot(f, abs(Y/N)); % amplitude vs frequency
+semilogy(f, abs(Y/N)); % amplitude vs frequency
 % plot(f, abs(Y).^2); % power vs frequency
 title('FFT');
