f854548a589a01174f0e3bb4916960bfe99c1a61
pharapeti <patrice.harapeti@gmail.com>
2021-04-21 23:58:18 +1000

    Looks like motion is working

--- binary-star-system/binary_star_systems.m
+++ binary-star-system/binary_star_systems.m
@@ -26,10 +26,18 @@ star2 = Body(star2_mass, star2_initial_position, star2_initial_velocity);
 starsInSystem = [star1, star2];
 
 solution = generateNumericalSolution(starsInSystem, timeSeries, timestep);
+star_1_motion = solution(:, 1:4);
+star_2_motion = solution(:, 5: 8);
 
-% subplot(1, 8, 1);
-% plot(timeSeries, positionXOf1);
-% title('PosX of 1');
+figure(1);
+plot3(timeSeries, star_1_motion(:, 1), star_1_motion(:, 2));
+grid on;
+title('Star 1 - X,Y vs Time');
+
+figure(2);
+plot3(timeSeries, star_2_motion(:, 1), star_2_motion(:, 2));
+grid on;
+title('Star 2 - X,Y vs Time');
 % 
 % subplot(1, 8, 2);
 % plot(timeSeries, positionXOf2);
@@ -61,30 +69,32 @@ solution = generateNumericalSolution(starsInSystem, timeSeries, timestep);
 
 % This function computes a numerical solution for a given dataset via the
 % Euler's method.
-function a = generateNumericalSolution(starsInSystem, timeSeries, stepSize)
-    a = 1;
-    % Generate vectors
-    positionXOf1 = nan(size(timeSeries));
-    positionXOf2 = nan(size(timeSeries));
-    positionYOf1 = nan(size(timeSeries));
-    positionYOf2 = nan(size(timeSeries));
-    
-    velocityXOf1 = nan(size(timeSeries));
-    velocityXOf2 = nan(size(timeSeries));
-    velocityYOf1 = nan(size(timeSeries));
-    velocityYOf2 = nan(size(timeSeries));
+function starMotionArray = generateNumericalSolution(starsInSystem, timeSeries, stepSize)
+    % Stars
+    star1 = starsInSystem(1);
+    star2 = starsInSystem(2);
 
-    % Define initial values
-    positionXOf1(1) = 10;
-    positionXOf2(1) = 2;
-    positionYOf1(1) = 10;
-    positionYOf2(1) = 2;
+    % Generate data structure for each star
+    % Rows represent time steps
+    star1_Motion = nan(length(timeSeries), 4);
+    star2_Motion = nan(length(timeSeries), 4);
     
-    velocityXOf1(1) = 0;
-    velocityXOf2(1) = 0;
-    velocityYOf1(1) = 0;
-    velocityYOf2(1) = 0;
+    % Column 1 is position in x direction
+    star1_Motion(1, 1) = star1.Position(1);
+    star2_Motion(1, 1) = star2.Position(1);
     
+    % Column 2 is position in y direction
+    star1_Motion(1, 2) = star1.Position(2);
+    star2_Motion(1, 2) = star2.Position(2);
+    
+    % Column 3 is velocity in x direction
+    star1_Motion(1, 3) = star1.Velocity(1);
+    star2_Motion(1, 3) = star2.Velocity(1);
+    
+    % Column 4 is velocity in y direction
+    star1_Motion(1, 4) = star1.Velocity(2);
+    star2_Motion(1, 4) = star2.Velocity(2);
+
     % Stars
     star1 = starsInSystem(1);
     star2 = starsInSystem(2);
@@ -98,49 +108,62 @@ function a = generateNumericalSolution(starsInSystem, timeSeries, stepSize)
     % function arguments
     for i = 1:length(timeSeries) - 1
         % Calculate distance between star 1 and 2
-        
         vector_r = calculateDistanceBetweenStars(star1, star2);
         vector_r_length = norm(vector_r);
         unit_vector_r = vector_r ./ vector_r_length;
 
+        % Using distance, calculate force on each star
         forceOn1 = (g_factor ./ (vector_r_length .^ 2)) .* unit_vector_r;
+        
+        % Split forces into (x, y) directions
         forceXOn1 = forceOn1(1);
         forceYOn1 = forceOn1(2);
 
+        % Newton's Second Law
+        % Equal and opposite reaction force
         forceXOn2 = - forceXOn1;
         forceYOn2 = - forceYOn1;
 
+        % Using force on each mass, determine accelerations on each star in
+        % (x, y) directions
         accelerationXOf1 = forceXOn1 ./ star1.Mass;
         accelerationYOf1 = forceYOn1 ./ star1.Mass;
 
         accelerationXOf2 = forceXOn2 ./ star2.Mass;
         accelerationYOf2 = forceYOn2 ./ star2.Mass;
         
-        velocityXOf1(i + 1) = velocityXOf1(i) + (stepSize .* accelerationXOf1);
-        velocityYOf1(i + 1) = velocityYOf1(i) + (stepSize .* accelerationYOf1);
-
-        velocityXOf1(i + 1) = velocityXOf1(i) + (stepSize .* accelerationXOf2);
-        velocityYOf2(i + 1) = velocityYOf2(i) + (stepSize .* accelerationYOf2);
-
-        positionXOf1(i + 1) = positionXOf1(i) + (stepSize .* velocityXOf1(i));
-        positionYOf1(i + 1) = positionYOf1(i) + (stepSize .* velocityYOf1(i));
+        % Update Velocities
+        % Star 1
+        star1_Motion(i + 1, 3) = star1_Motion(i, 3) + (stepSize .* accelerationXOf1);
+        star1_Motion(i + 1, 4) = star1_Motion(i, 4) + (stepSize .* accelerationYOf1);
         
-        positionXOf1(i + 1) = positionXOf2(i) + (stepSize .* velocityXOf2(i));
-        positionYOf2(i + 1) = positionYOf2(i) + (stepSize .* velocityYOf2(i));
-%       velocity(i + 1) = velocity(i) + (stepSize .* acceleration);
-%       position(i + 1) = position(i) + (stepSize * velocity(i));
+        % Star 2
+        star2_Motion(i + 1, 3) = star2_Motion(i, 3) + (stepSize .* accelerationXOf2);
+        star2_Motion(i + 1, 4) = star2_Motion(i, 4) + (stepSize .* accelerationYOf2);
+
+        % Update Positions
+        % Star 1
+        star1_Motion(i + 1, 1) = star1_Motion(i, 1) + (stepSize .* star1_Motion(i, 3));
+        star1_Motion(i + 1, 2) = star1_Motion(i, 2) + (stepSize .* star1_Motion(i, 4));
+        
+        % Star 2
+        star2_Motion(i + 1, 1) = star2_Motion(i, 1) + (stepSize .* star2_Motion(i, 3));
+        star2_Motion(i + 1, 2) = star2_Motion(i, 2) + (stepSize .* star2_Motion(i, 4));
     end
+
+    % Package and return motions of stars
+    starMotionArray = [star1_Motion, star2_Motion];
 end
 
 % Returns 1x2 array of distances in (x, y) respectively
-function r = calculateDistanceBetweenStars(starA, starB)
+function r = calculateDistanceBetweenStars(star_A, star_B)
     % TODO just reference these directly instead of assigned to temporary
     % variable
-    starA_x = starA.Position(1);
-    starA_y = starA.Position(2);
+    starA_x = star_A.Position(1);
+    starA_y = star_A.Position(2);
     
-    starB_x = starB.Position(1);
-    starB_y = starB.Position(2);
+    starB_x = star_B.Position(1);
+    starB_y = star_B.Position(2);
 
     r = [starB_x - starA_x, starB_y - starA_y];
 end
