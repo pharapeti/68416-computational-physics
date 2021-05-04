fdf519b099a276b0f323ee35beafbc9c37d2cf0a
pharapeti <patrice.harapeti@gmail.com>
2021-05-04 11:29:13 +1000

    Model validation using final position of Moon

--- binary-star-system/binary_star_systems.m
+++ binary-star-system/binary_star_systems.m
@@ -3,8 +3,8 @@ clear; clc; close all
 
 %% Set up Simulation
 % Simulation Parameters
-timestep = 120; % timestep (seconds)
-totalTime = 60 * 60 * 24 * 30; % total time of simulation (seconds)
+timestep = 60; % timestep (seconds)
+totalTime = 60 * 60 * 24 * 27; % total time of simulation (seconds)
 timeSeries = 0:timestep:totalTime;
 G = 6.67408 * 10.^-11;
 
@@ -13,8 +13,12 @@ simulation = Simulation(timestep, totalTime, G);
 
 % Create Star 1
 star1_mass = 5.97219 * 10.^24; % mass (kg)
-star1_initial_position = [4000, 0]; % position (m)
-star1_initial_velocity = [0, 0]; % velocity (m/s)
+
+% Earth's COM -> Barycenter = 4675km
+star1_initial_position = [-4675 * 10.^3, 0]; % position (m)
+
+% Earth orbit's the Earth-Moon Barycenter at 45km/h = 12.5m/ss
+star1_initial_velocity = [0, 12.5]; % velocity (m/s)
 simulation.createBody(star1_mass, star1_initial_position, star1_initial_velocity);
 
 % Create Star 2
@@ -59,7 +63,7 @@ ylabel('Position Y');
 figure(4);
 plot(simulation.TimeSeries, simulation.Bodies(1).PE);
 hold on;
-plot(simulation.TimeSeries, simulation.Bodies(2).KE, 'r');
+plot(simulation.TimeSeries, simulation.Bodies(1).KE, 'r');
 % xlim([-10.^6, 10.^6]);
 % ylim([-10.^6, 10.^6]);
 title('KE and PE of Earth');
@@ -77,6 +81,80 @@ title('KE and PE of Moon');
 legend('PE', 'KE');
 xlabel('Time (s)');
 ylabel('Energy (j)');
+
+figure(6);
+plot(simulation.TimeSeries, simulation.Bodies(1).PE);
+% xlim([-10.^6, 10.^6]);
+% ylim([-10.^6, 10.^6]);
+title('PE of Moon');
+legend('PE');
+xlabel('Time (s)');
+ylabel('Energy (j)');
+
+figure(7);
+plot(simulation.TimeSeries, simulation.Bodies(2).PE);
+% xlim([-10.^6, 10.^6]);
+% ylim([-10.^6, 10.^6]);
+title('PE of Earth');
+legend('PE');
+xlabel('Time (s)');
+ylabel('Energy (j)');
+
+%% Validate Results with Energy Method
+
+% Determine last index of simulation timeseries
+final_time_index = length(simulation.TimeSeries) - 1;
+
+% Compare total energy of system at first timestep to the total energy of
+% the system at the final timestep
+initial_energy = totalEnergyOfSimulationAtI(1, simulation);
+final_energy = totalEnergyOfSimulationAtI(final_time_index, simulation);
+delta_E = abs(final_energy - initial_energy);
+
+% Find the relative error % between the two
+relative_error = delta_E ./ final_energy;
+
+% Do this for a range of step sizes to prove that minimising the step size
+% also minimises the relative error
+
+%% Validate Results with final position of the Moon convergence
+
+step_sizes = logspace(2, 4);
+final_positions_of_moon = nan(length(step_sizes), 2);
+
+for i = 1:length(step_sizes)
+    % Setup simulation at a given timestep
+    sim = Simulation(step_sizes(i), totalTime, G);
+    sim.createBody(star1_mass, star1_initial_position, star1_initial_velocity);
+    sim.createBody(star2_mass, star2_initial_position, star2_initial_velocity);
+
+    % Solve system numerically
+    solveSystemNumerically(sim);
+
+    % Determine final position of the Moon
+    final_pos_x = simulation.Bodies(2).Position.X(final_time_index);
+    final_pos_y = simulation.Bodies(2).Position.Y(final_time_index);
+    final_positions_of_moon(i, :) = [final_pos_x, final_pos_y];
+end
+
+% Plot Step size vs Final Position of Moon
+figure(8)
+loglog(step_sizes, final_positions_of_moon(:, 1));
+title('Position X of Moon at end of simulation vs Step Size');
+xlabel('Step Size (log)');
+ylabel('Position of Moon X');
+legend('Position of Moon X');
+
+% Plot Step size vs Final Position of Moon
+figure(9)
+loglog(step_sizes, final_positions_of_moon(:, 2));
+title('Position Y of Moon at end of simulation vs Step Size');
+xlabel('Step Size (log)');
+ylabel('Position of Moon Y');
+legend('Position of Moon Y');
+
+% Do this for a range of step sizes to prove that minimising the step size
+% also minimises the relative error
 
 %% Functions
 
@@ -211,4 +289,14 @@ function PE = calculatePE(i, bodyA, bodyB, G)
     r = calculateDistanceBetweenBodies(i, bodyA, bodyB);
     r_length = norm(r);
     PE = (-G .* bodyA.Mass .* bodyB.Mass) ./ r_length;
+end
+
+function E = totalEnergyOfSimulationAtI(i, simulation)
+    E = 0;
+
+    for j = 1:length(simulation.Bodies) - 1
+        PE = simulation.Bodies(j).PE(i);
+        KE = simulation.Bodies(j).KE(i);
+        E = E + PE + KE;
+    end
 end
